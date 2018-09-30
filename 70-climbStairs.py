028ff4bf26aa384a270d66e16506577df3a51dd1
sunshinewxz <zixinwangxinze@qq.com>
2018-09-29 21:35:52 -0400

    70. Climbing Stairs
    You are climbing a stair case. It takes n steps to reach to the top.
    
    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
    
    Note: Given n will be a positive integer.
    
    Example 1:
    
    Input: 2
    Output: 2
    Explanation: There are two ways to climb to the top.
    1. 1 step + 1 step
    2. 2 steps
    Example 2:
    
    Input: 3
    Output: 3
    Explanation: There are three ways to climb to the top.
    1. 1 step + 1 step + 1 step
    2. 1 step + 2 steps
    3. 2 steps + 1 step

---
+++ 70-climbStairs.py
@@ -0,0 +1,21 @@
+from scipy.special import comb
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+        	return 1
+        # if n == 2:
+        # 	return 2
+        result = 1
+        temp = n
+        while(temp >= 2):
+        	result += comb(n-1, temp-2)
+        	n = n-1
+        	temp = temp-2
+        return result
+
+s = Solution()
+print(s.climbStairs(2))
