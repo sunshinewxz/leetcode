ff24a8a18eb8f65905b0be34fd1b76d7b7772904
sunshinewxz <zixinwangxinze@qq.com>
2018-10-17 20:52:38 -0400

    103. Binary Tree Zigzag Level Order Traversal
    Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
    
    For example:
    Given binary tree [3,9,20,null,null,15,7],
        3
       / \
      9  20
        /  \
       15   7
    return its zigzag level order traversal as:
    [
      [3],
      [20,9],
      [15,7]
    ]

--- 103-zigzagLevelOrder.py
+++ 103-zigzagLevelOrder.py
@@ -1,8 +1,9 @@
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def zigzagLevelOrder(self, root):
@@ -10,46 +11,33 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        # curr_layer and next_layer save the node information
+        queue = []
         if root is None:
-        	return
-        curr_layer = []
-        next_layer = []
-        temp = []
+            return[]
         result = []
-        curr_layer.append(root)
-        need_rever = False
-        while(len(curr_layer) > 0):
-        	for node in curr_layer:
-        		if node is not None:
-        			temp.append(node.val)
-	        		if node.left is not None:
-	        			next_layer.append(node.left)
-	        		if node.right is not None:
-	        			next_layer.append(node.right)
-        	if need_rever:
-        		temp.reverse()
-        		need_rever = False
-        	else:
-        		need_rever = True
-        	result.append(temp)
-        	print(temp)
-        	curr_layer = next_layer
-        	next_layer = []
-        	temp = []
-        return result
-
-node1 = TreeNode(3)
-node2 = TreeNode(9)
-node3 = TreeNode(20)
-node4 = TreeNode(15)
-node5 = TreeNode(7)
-node1.left = node2
-node1.right = node3
-node2.left = None
-node2.right = None
-node3.left = node4
-node3.right = node5
-result = Solution()
-print(result.zigzagLevelOrder(node1))
-
+        flag = 0
+        queue.append(root)
+        return self.layerSet(queue, result, flag)
+    
+    def layerSet(self, queue, result, flag):
+        if len(queue)==0:
+            return result
+        temp = []
+        length = len(queue)
+        for i in range(length):
+            node = queue.pop(0)
+            temp.append(node.val)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+        if flag == 0:
+            result.append(temp)
+        else:
+            temp.reverse()
+            result.append(temp)
+        return self.layerSet(queue, result, 1-flag)
+            
+            
+        
+        
