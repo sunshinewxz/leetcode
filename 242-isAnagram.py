f2f97445b99b3944304edf5c4a6f2e73b25b9f4b
sunshinewxz <zixinwangxinze@qq.com>
2018-11-08 19:59:09 -0500

    242. Valid Anagram
    Given two strings s and t , write a function to determine if t is an anagram of s.
    
    Example 1:
    
    Input: s = "anagram", t = "nagaram"
    Output: true
    Example 2:
    
    Input: s = "rat", t = "car"
    Output: false
    Note:
    You may assume the string contains only lowercase alphabets.
    
    Follow up:
    What if the inputs contain unicode characters? How would you adapt your solution to such case?

---
+++ 242-isAnagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = sorted(s)
+        t = sorted(t)
+        return s == t
