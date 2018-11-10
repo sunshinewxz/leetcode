b2b1011a97d3bb047d1ea90cb91078db99730b1f
sunshinewxz <zixinwangxinze@qq.com>
2018-11-10 11:19:27 -0500

    Update 287-findDuplicate.py

--- 287-findDuplicate.py
+++ 287-findDuplicate.py
@@ -8,3 +8,22 @@ class Solution(object):
         sum2 = sum(set(nums))
         num_du = len(nums) - len(set(nums))
         return (sum1-sum2)/num_du
+    
+# solution 2:
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        fast = nums[nums[0]]
+        slow = nums[0]
+        while(fast != slow):
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+        
+        fast = 0
+        while(fast != slow):
+            fast = nums[fast]
+            slow = nums[slow]
+        return slow
