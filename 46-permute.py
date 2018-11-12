c3ac2d13ea2d650f3231ea9fd3b4b793eaca768b
sunshinewxz <zixinwangxinze@qq.com>
2018-11-11 20:08:40 -0500

    Update 46-permute.py

--- 46-permute.py
+++ 46-permute.py
@@ -15,3 +15,21 @@ class Solution(object):
         result = []
         backtrack(0, len(nums))
         return result
+    
+# solution 2
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.dfs(nums, result, [])
+        return result
+        
+    def dfs(self, nums, result, temp):
+        if len(nums) == 0:
+            result.append(temp)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], result, temp+[nums[i]])
+            
