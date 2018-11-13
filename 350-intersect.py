b76a841eeb9f829ea2c4c68a38db9aacad88e2d5
sunshinewxz <zixinwangxinze@qq.com>
2018-11-12 19:24:12 -0500

    350. Intersection of Two Arrays II
    Given two arrays, write a function to compute their intersection.
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2,2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [4,9]
    Note:
    
    Each element in the result should appear as many times as it shows in both arrays.
    The result can be in any order.
    Follow up:
    
    What if the given array is already sorted? How would you optimize your algorithm?
    What if nums1's size is small compared to nums2's size? Which algorithm is better?
    What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
    
    If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, read chunks of array that fit into the memory, and record the intersections.
    
    If both nums1 and nums2 are so huge that neither fit into the memory, sort them individually (external sort), then read 2 elements from each array at a time in memory, record intersections.

---
+++ 350-intersect.py
@@ -0,0 +1,38 @@
+# solution 1: dictionary - Counter
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        count1 = collections.Counter(nums1)
+        result = []
+        for i,n in enumerate(nums2):
+            if n in count1 and count1[n] > 0:
+                result.append(n)
+                count1[n] -= 1
+        return result
+
+# solution 2: two pointer
+class Solution(object):
+    def intersect(self, nums1, nums2):
+
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        pt1 = pt2 = 0
+        res = []
+
+        while True:
+            try:
+                if nums1[pt1] > nums2[pt2]:
+                    pt2 += 1
+                elif nums1[pt1] < nums2[pt2]:
+                    pt1 += 1
+                else:
+                    res.append(nums1[pt1])
+                    pt1 += 1
+                    pt2 += 1
+            except IndexError:
+                break
+
+        return res
