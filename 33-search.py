6a5da216349f96e7cc1518e9a8d630e264a423c2
sunshinewxz <zixinwangxinze@qq.com>
2018-11-13 09:23:37 -0500

    33. Search in Rotated Sorted Array
    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
    
    (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
    
    You are given a target value to search. If found in the array return its index, otherwise return -1.
    
    You may assume no duplicate exists in the array.
    
    Your algorithm's runtime complexity must be in the order of O(log n).
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1

--- 33-search.py
+++ 33-search.py
@@ -1,35 +1,25 @@
-class Solution(object):
+class Solution:
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        if len(nums) == 0:
-            return -1
-        start = 0
-        end = len(nums)-1
-        while(start < end):
-            mid = (start + end)//2
+        start, end = 0, len(nums)-1
+        while(start <= end):
+            mid = (start + end) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < nums[end]:
-                if target in nums[mid:end+1]:
+            if nums[mid] <= nums[end]:
+                if nums[mid] < target <= nums[end]:
                     start = mid+1
                 else:
                     end = mid-1
-            elif nums[mid] > nums[end]:
-                if target in nums[start:mid+1]:
-                    end = mid
+            elif nums[mid] >= nums[start]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid-1
                 else:
                     start = mid+1
-            else:
-                end = mid-1
-        if nums[start] == target:
-            return start
-        elif nums[end] == target:
-            return end
         return -1
-
-s = Solution()
-print(s.search([4,5,6,7,0,1,2],3))
+                    
+                    
